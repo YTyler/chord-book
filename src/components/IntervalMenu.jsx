eafcc9eff658b51f9e0d7ada33162aac6661b303
Tyler C. Yates <tyates907@gmail.com>
2020-03-09 00:07:05 -0700

    add titles and dropdown

--- src/components/IntervalMenu.jsx
+++ src/components/IntervalMenu.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function IntervalMenu() {
   const intervalMenuStyle = {
+    textAlign: 'left',
     border: '2px solid black',
     borderRadius: '10px',
+    padding: '15px',
     minHeight: '300px',
     minWidth: '600px',
     marginLeft: '5px'
@@ -11,6 +13,17 @@ function IntervalMenu() {
   return(
     <div style={intervalMenuStyle}>
       <h1>Interval Menu</h1>
+      <h2>Root Note: C</h2>
+      <select id="interval">
+        <option value="1">Unison</option>
+        <option value="2">2nd</option>
+        <option value="3">3rd</option>
+        <option value="4">4th</option>
+        <option value="5">5th</option>
+        <option value="6">6th</option>
+        <option value="7">7th</option>
+        <option value="8">Octave</option>
+      </select>
     </div>
   )
 }
