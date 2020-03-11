fc402d0ae8ced649301fbb5e5e48aceda8ef46d4
Tyler C. Yates <tyates907@gmail.com>
2020-03-11 14:12:10 -0700

    create Major component

---
+++ src/components/Major.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Major() {
+
+  //localStyles
+
+    return (
+      <div className = 'rightMenu'>
+        <h1> Major Menu</h1>
+      </div>
+    );
+}
+
+export default Major;
