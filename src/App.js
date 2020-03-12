b7ca76aa7eb31cb1ce329745947ba7607a7ce0ec
Tyler C. Yates <tyates907@gmail.com>
2020-03-11 18:35:59 -0700

    add brackets to child comments

--- src/App.js
+++ src/App.js
@@ -28,24 +28,30 @@ function App(props) {
             <RootMenu setCurrentRoot={setCurrentRoot}/>
           </div>
           <Switch>
-            //Menu Routes
+            {//Menu Routes
+            }
             <Route exact path='/' render={()=><NoteMenu currentRoot={currentRoot} />} />
             <Route exact path='/chords' render={()=><ChordList currentRoot={currentRoot} />} />
             <Route exact path='/scales' render={()=><ScaleList currentRoot={currentRoot} />} />
             <Route exact path='/intervals' render={()=><IntervalMenu currentRoot={currentRoot} />} />
-            //Scale Routes
+            {//Scale Routes
+            }
             <Route exact path="/scales/ionian" render={''} />
             <Route exact path="/scales/aeolian" render={''} />
+            {
             // <Route exact path="/scales/harmonic" render={''} />
             // <Route exact path="/scales/melodic" render={''} />
             // <Route exact path="/scales/pentatonic-minor" render={''} />
             // <Route exact path="/scales/blues" render={''} />
             // <Route exact path="/scales/pentatonic-major" render={''} />
             // <Route exact path="/scales/bebop" render={''} />
+            }
 
-            //Chord Routes
+            {//Chord Routes
+            }
             <Route exact path="/chords/major" render={()=><MajorChord currentRoot={currentRoot}/>} />
             <Route exact path="/chords/minor" render={()=><MinorChord currentRoot={currentRoot}/>} />
+            {
             // <Route exact path="/chords/7ths" render={''} />
             // <Route exact path="/chords/maj7" render={''} />
             // <Route exact path="/chords/maj7" render={''} />
@@ -53,6 +59,7 @@ function App(props) {
             // <Route exact path="/chords/dim" render={''} />
             // <Route exact path="/chords/half-dim" render={''} />
             // <Route exact path="/chords/aug" render={''} />
+            }
           </Switch>
         </div>
       </div>
