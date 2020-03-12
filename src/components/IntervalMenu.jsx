78a73d37f698520307bd2d7bd4ea7b90aa9090a6
Tyler C. Yates <tyates907@gmail.com>
2020-03-11 23:30:14 -0700

    update from old state to state hooks

--- src/components/IntervalMenu.jsx
+++ src/components/IntervalMenu.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
+import { Note } from '@tonaljs/tonal'
 
 const intervalMenuStyle = {
   backgroundColor: 'white',
@@ -12,31 +13,25 @@ const intervalMenuStyle = {
   marginLeft: '5px'
 }
 
-class IntervalMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {interval: 1};
-
-    this.changeInterval = this.changeInterval.bind(this);
-    this.submitInterval = this.submitInterval.bind(this);
-  }
+function IntervalMenu(props) {
+  
+  const [interval, setInterval] = useState(1);
 
   //state functions
-  changeInterval(event) {
-    this.setState({interval: event.target.value})
-  }
-  submitInterval(event) {
-    alert('Your Choice was ' + this.state.interval)
-    event.preventDefault();
+  // changeInterval(event) {
+  //   this.setState({interval: event.target.value})
+  // }
+  const submitInterval = () => {
+    console.log(interval);
+    console.log(props.currentRoot);
   }
 
-  render() {
   return(
     <div style={intervalMenuStyle}>
       <h1>Interval Menu</h1>
-      <h2>Root Note: C</h2>
-      <form onSubmit = {this.submitInterval}>
-        <select value={this.state.value} onChange={this.changeInterval} >
+      <h2>Root Note: {props.currentRoot}</h2>
+      <form onSubmit = {submitInterval()}>
+        <select value={interval} onChange={e => setInterval(e.currentTarget.value)} >
           <option value={1}>Unison</option>
           <option value={2}>2nd</option>
           <option value={3}>3rd</option>
@@ -53,7 +48,6 @@ class IntervalMenu extends React.Component {
       <Link to='/'>Main Menu</Link>
     </div>
   )
-}
 }
 
 export default IntervalMenu;
